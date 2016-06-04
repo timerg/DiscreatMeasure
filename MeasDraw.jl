9897d04d4ec80f92546149428fc03cc1778cc120
timerg <ggteven@gmail.com>
2016-06-04 16:46:52 +0800

    e plot done

--- MeasDraw.jl
+++ MeasDraw.jl
@@ -6,51 +6,74 @@ using DataFrames
 inputIdVg = readtable("IdVg", separator=',', header=true)
 
 ### Concat
-a = readtable("testa.csv", separator=',', header=false)
-b = readtable("testb.csv", separator=',', header=false)
-c = readtable("testc.csv", separator=',', header=false)
-d = readtable("testd.csv", separator=',', header=false)
-
+a  = readtable("./data/scope_0.csv", separator=',', header=true, skipstart=1)
+b  = readtable("./data/scope_2.csv", separator=',', header=true, skipstart=1)
+b2 = readtable("./data/scope_3.csv", separator=',', header=true, skipstart=1)
+c1 = readtable("./data/scope_4.csv", separator=',', header=true, skipstart=1)
+c2 = readtable("./data/scope_5.csv", separator=',', header=true, skipstart=1)
+c3 = readtable("./data/scope_6.csv", separator=',', header=true, skipstart=1)
+c4 = readtable("./data/scope_7.csv", separator=',', header=true, skipstart=1)
+c5 = readtable("./data/scope_8.csv", separator=',', header=true, skipstart=1)
+c6 = readtable("./data/scope_9.csv", separator=',', header=true, skipstart=1)
+d1 = readtable("./data/scope_10.csv", separator=',', header=true, skipstart=1)
+d2 = readtable("./data/scope_11.csv", separator=',', header=true, skipstart=1)
+e1 = readtable("./data/scope_12.csv", separator=',', header=true, skipstart=1)
+e2 = readtable("./data/scope_13.csv", separator=',', header=true, skipstart=1)
 
 function jointwice(s1, s2)
-    vcat(join(s1, s2, on = [:x1], kind = :anti), s2)
+    vcat(join(s1, s2, on = [:second], kind = :anti), s2)
 end
 
 
-function myConcat(s1, s2, s3...)
 
-    out = jointwice(s1, s2)
-    if length(s3) <= 1
-        out = jointwice(out, s3[1])
+function myConcat(s1, s2...)
+    out = jointwice(s1, s2[1])
+    if length(s2) <= 1
+        return out
     else
-        for i = 1:length(s3)
-            out = jointwice(out, s3[i])
+        for i = 2:length(s2)
+            out = jointwice(out, s2[i])
         end
+        return out
     end
-    return out
 end
+
 ### RealTime
-input_all = myConcat(a, b, c, d)
+
+inpute_ = myConcat(e1, e2)
+inpute = inpute_[800:1741, :]
 
 ##### need input_time, input_val
-input_v = input_all[:x2]
-input_time = input_all[:x1]
+# input_v = input_all[:x2]
+# input_time = input_all[:x1]
+#
+Fe = fft(inpute[:Volt])
+Fe = fftshift(Fe)
+# inpute_f = ./(1, (inpute[:second] - middle(inpute[:second])))
+maxfe = round( 1/abs(inpute[:second][2] - inpute[:second][1]))
+minfe = 1/(inpute[:second][end] - inpute[:second][1])
+inpute_f = [minfe : round((maxfe - minfe)/(length(inpute[:second]) - 1),5) : maxfe]
+inpute_all = DataFrame(xf = inpute_f, xt = inpute[:second], F = abs(Fe), T = inpute[:Volt])
 
-F = fft(input_v)
-F = fftshift(F)
-input_f = ./(1, (input_time - middle(input_time)))
-input = DataFrame(xf = input_f, xt = input_time, F = abs(F), T = input_v)
 
 
 
-
-plot_f = plot(input, x = collect(1:length(input[:xf])), y = "F"
-    , Geom.line, Geom.point
-    , Scale.x_log10)
-
-plot_IdVg = plot(x = inputIdVg[:Vgs], y = inputIdVg[:Id], Geom.point
-    # ,Scale.y_log10
+plot_f = plot(inpute_all[length(inpute_all[:F])/2 : end, :]
+    , x = "xf"
+    , y = "F"
+    , Geom.line
+    # , Scale.x_log10
+    , Scale.y_log10
+    # , Guide.xticks(ticks = [0:4, 1.781])
+    # , Guide.xticks(ticks = [[1, 10, 100, 200, 300, 400, 500], 60])
     )
+
+# plot_IdVg = plot(x = inputIdVg[:Vgs], y = inputIdVg[:Id], Geom.point
+#     # ,Scale.y_log10
+#     )
+
+plot(inpute, x = "second", y = "Volt", Geom.line)
+
 
 
 
